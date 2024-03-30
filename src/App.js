701cadb34401ccd85d18b0049116cde2f875ada1
Shital Kumar Awal <shresthashital706@gmail.com>
2024-03-30 10:55:33 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './index.css';
+ 
+ 
+
+  const App = () => {
+  const [diceNumber, setDiceNumber] = useState(2);
+  const [diceNumber2, setDiceNumber2] = useState(2);
+  
+
+  const refreshDice = () => {
+    const ranno = Math.floor(Math.random()*6)+1
+    setDiceNumber(ranno)
+    const ranno2 = Math.floor(Math.random()*6)+1
+    setDiceNumber2(ranno2)
+  };
+
+  return (
+    <div >
+      <center>
+      <img  width={300} height ={300} src ={require(`./assets/${diceNumber}.png`)}></img>
+      <img  width={300} height ={300} src ={require(`./assets/${diceNumber2}.png`)}></img>
+     
+      <br/>
+      <button onClick={()=>refreshDice()} className='button'>Roll</button>
+      </center>
+     
+      </div>
+    
+  );
+  }
+
+export default App;
+
+    
